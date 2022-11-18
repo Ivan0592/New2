dd6714ce873f7a442bdd36235bb6bc5d88acbcd3
Ivan <ivanvas05@mail.ru>
2022-11-18 07:33:18 +0300

    task2 refresh

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,22 +3,32 @@
 // 3 -> Среда
 // 5 -> Пятница
 
-Console.WriteLine("Ввести число: ");
-
-int понедельник = 1;
-int вторник = 2;
-int среда = 3;
-int четверг = 4;
-int пятница = 5;
-int суббота = 6;
-int воскресенье = 7;
+Console.WriteLine("Ввести число от 1 до 7: ");
 int сегодня = Convert.ToInt32(Console.ReadLine());
 
-if(сегодня == понедельник) Console.WriteLine("Сегодня понедельник");
-if(сегодня == вторник) Console.WriteLine("Сегодня вторник");
-if(сегодня == среда) Console.WriteLine("Сегодня среда");
-if(сегодня == четверг) Console.WriteLine("Сегодня четверг");
-if(сегодня == пятница) Console.WriteLine("Сегодня пятница");
-if(сегодня == суббота) Console.WriteLine("Сегодня суббота");
-if(сегодня == воскресенье) Console.WriteLine("Сегодня воскресенье");
+if(сегодня == 1) {
+    Console.WriteLine("Сегодня понедельник");
+}
+else if(сегодня == 2) {
+    Console.WriteLine("Сегодня вторник");
+}
+else if(сегодня == 3) {
+    Console.WriteLine("Сегодня среда");
+}
+else if(сегодня == 4) {
+    Console.WriteLine("Сегодня четверг");
+}
+else if(сегодня == 5) {
+    Console.WriteLine("Сегодня пятница");
+}
+else if(сегодня == 6) {
+    Console.WriteLine("Сегодня суббота");
+}
+else if(сегодня == 7) {
+    Console.WriteLine("Сегодня воскресенье");
+}
+else {
+Console.WriteLine("Дней всего 7!");
+}
+
 
