a5d3399579ba86c9291a5d83138e13c4d825f595
Ivan <ivanvas05@mail.ru>
2022-11-29 15:52:33 +0300

    task16

--- task16/Program.cs
+++ task16/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Решение в группах задач:
+// Задача 28: Напишите программу,
+// которая принимает на вход число N и
+// выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int Factorial(int number)
+{
+    int factorial = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        factorial = factorial *i;
+    
+    }
+    return factorial;
+}
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int factorial = Factorial(A);
+Console.WriteLine("Сумма чисел равна: " + factorial);
