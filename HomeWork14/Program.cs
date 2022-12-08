12c23fc7910b3181baf7521b7d1cc13929476b77
Ivan <ivanvas05@mail.ru>
2022-12-08 21:40:34 +0300

    HW14

---
+++ HomeWork14/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int[] FillArray(int size)
+{
+    int[] arr = new int[size];
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr [i] = new Random().Next(-10000, 10000);
+    
+    }
+    return arr;
+}
+Console.WriteLine("Введите кол-во чисел: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(num);
+Console.WriteLine(string.Join(", ", array));
+int count = 0;
+
+for(int i = 0; i < array.Length; i++)
+{
+    if (array[i] >=0 & array[i] <= 10000)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Пользователем введено чисел больше нуля: {count} ");    
+
--- task20/Program.cs
+++ task20/Program.cs
@@ -6,6 +6,32 @@
 // [1 2 3 4 5] -> [5 4 3 2 1]
 // [6 7 3 6] -> [6 3 7 6]
 
+// int[] FillArray(int size)
+// {
+//     int[] arr = new int[size];
+//     for(int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = new Random().Next(-9,10);
+//     }
+//     return arr;
+// }
+// void ReversArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length/2; i++)
+//     {
+//         int temp = array[i];
+//         array[i] = array[array.Length - i - 1];
+//         array[array.Length - i - 1] = temp;
+//     }
+// }
+// Console.WriteLine("Введите длину массива: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int[] array = FillArray(num);
+// Console.WriteLine(string.Join(", ", array));
+// ReversArray(array);
+// Console.WriteLine(string.Join(", ", array));
+
+
 int[] FillArray(int size)
 {
     int[] arr = new int[size];
@@ -15,18 +41,17 @@ int[] FillArray(int size)
     }
     return arr;
 }
-void ReversArray(int[] array)
-{
-    for (int i = 0; i < array.Length/2; i++)
-    {
-        int temp = array[i];
-        array[i] = array[array.Length - i - 1];
-        array[array.Length - i - 1] = temp;
-    }
-}
+
+
 Console.WriteLine("Введите длину массива: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int[] array = FillArray(num);
 Console.WriteLine(string.Join(", ", array));
-ReversArray(array);
-Console.WriteLine(string.Join(", ", array));
+
+
+int[] newArray = new int[num];
+for(int i = 0; i < array.Length; i++)
+{
+    newArray[i] = array[i];
+}
+Console.WriteLine(string.Join(", ", newArray));
