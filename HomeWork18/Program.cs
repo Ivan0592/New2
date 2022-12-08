07838ad52c52cea46113dd9b5a51144de32c450d
Ivan <ivanvas05@mail.ru>
2022-12-09 02:04:26 +0300

    HW18

---
+++ HomeWork18/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.WriteLine("Введите размерность двумерного массива m x n");
+Console.Write("Введите m: ");
+int m =  Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n =  Convert.ToInt32(Console.ReadLine());
+
+double [,] matrix = new double [m,n];
+for(int i = 0; i < m; i++)
+{
+    for(int j = 0; j < n; j++)
+    {
+        matrix[i,j] = new Random().Next(0,11);
+        Console.Write(matrix[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+ Console.Write("Введите номер индекса строки: ");
+ int A = Convert.ToInt32(Console.ReadLine());
+ Console.Write("Введите номер индекса колонны: ");
+ int B = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+if(A < m && B < n)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Число " + matrix[A, B]);
+    Console.ForegroundColor = ConsoleColor.White;
+}
+    else Console.WriteLine("Такого элемента нет! ");
+
+for(int i = 0; i < m; i++)
+{
+    
+    for(int j = 0; j < n; j++)
+    {
+        if(i == A && j == B) Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write(matrix[i,j] + " ");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}    
+Console.WriteLine();
+
--- task22/Program.cs
+++ task22/Program.cs
@@ -16,17 +16,31 @@
 // }
 // Console.Write("Введите число ")
 // int mainNumber = Convert.ToInt32(Console.ReadLine());
-
-void BinaryView(int number)
+Console.WriteLine("Введите размеры массива");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
 {
-    if(number == 0)
-    {
-        return;
-    }
-    BinaryView(number/2);
-    Console.Write(number%2);
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
 }
-
-Console.WriteLine("Введите число" );
-int number = Convert.ToInt32(Console.ReadLine());
-BinaryView(number);
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t  ");
+        Console.WriteLine();
+}
+ 
+ Console.WriteLine("Введите координаты");
+ int a = Convert.ToInt32(Console.ReadLine());
+ int b = Convert.ToInt32(Console.ReadLine());
+ if (a>m && b>n)
+ Console.WriteLine("такого числа нет");
+ else
+ {
+ object c = array.GetValue(a,b);
+ Console.WriteLine(c);
+ }
