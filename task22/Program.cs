72439b0c7e1f2449b4c810351ac8caf08be05aca
Ivan <ivanvas05@mail.ru>
2022-12-08 20:50:32 +0300

    task22

---
+++ task22/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 42: Напишите программу, которая будет
+// преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+// int Recurse (int number)
+// {
+//     int result =number/2;
+//     while (result>0)
+//     {
+//     result = result -Recurse(result);
+
+//     }
+// }
+// Console.Write("Введите число ")
+// int mainNumber = Convert.ToInt32(Console.ReadLine());
+
+void BinaryView(int number)
+{
+    if(number == 0)
+    {
+        return;
+    }
+    BinaryView(number/2);
+    Console.Write(number%2);
+}
+
+Console.WriteLine("Введите число" );
+int number = Convert.ToInt32(Console.ReadLine());
+BinaryView(number);
